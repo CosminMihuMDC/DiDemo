d48be47b189c46afdb976a6614d44e729f8ac33a
Cosmin Mihu <cosmin.mihu@gmail.com>
2021-10-01 13:38:04 +0300

    init

---
+++ app/src/main/java/com/example/hiltdemo/MainRepository.kt
@@ -0,0 +1,5 @@
+package com.example.hiltdemo
+
+import androidx.lifecycle.ViewModel
+
+interface MainRepository
---
+++ app/src/main/java/com/example/hiltdemo/MainViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.hiltdemo
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel(repository: MainRepository) : ViewModel()
