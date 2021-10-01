d2da2c88c25639281026473a5823f224dc838fdf
Cosmin Mihu <cosmin.mihu@gmail.com>
2021-10-01 14:53:03 +0300

    Make repo singleton in singleton component

--- app/src/main/java/com/example/hiltdemo/di/DI.kt
+++ app/src/main/java/com/example/hiltdemo/di/DI.kt
@@ -6,11 +6,13 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DataModule {
 
     @Provides
+    @Singleton
     fun provideMainRepository(): MainRepository = MainRepositoryImpl()
 }
