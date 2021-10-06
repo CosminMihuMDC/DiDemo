84ce15ba394fbe04c55e3bd1e02ec4a4e87637a5
Cosmin Mihu <cosmin.mihu@gmail.com>
2021-10-06 16:36:39 +0300

    Fix

--- app/src/main/java/com/example/hiltdemo/activities/UserActivity.kt
+++ app/src/main/java/com/example/hiltdemo/activities/UserActivity.kt
@@ -10,16 +10,18 @@ import com.example.hiltdemo.data.SingletonRepository
 import com.example.hiltdemo.data.UserRepository
 import com.example.hiltdemo.di.USER_SCOPE_ID
 import com.example.hiltdemo.ui.theme.HiltDemoTheme
-import org.koin.android.ext.android.getKoin
 import org.koin.android.ext.android.inject
+import org.koin.core.component.KoinScopeComponent
+import org.koin.core.component.get
 import org.koin.core.parameter.parametersOf
+import org.koin.core.scope.Scope
 
-class UserActivity : ComponentActivity() {
+class UserActivity : ComponentActivity(), KoinScopeComponent {
 
-    private val userScope = getKoin().getScope(USER_SCOPE_ID)
+    override val scope: Scope
+        get() = getKoin().getScope(USER_SCOPE_ID)
 
-    private val userRepository: UserRepository
-            by userScope.inject(parameters = { parametersOf(1234L) })
+    private val userRepository: UserRepository by inject(parameters = { parametersOf(1234L) })
 
     private val singletonRepository: SingletonRepository by inject()
 
@@ -40,5 +42,9 @@ class UserActivity : ComponentActivity() {
                 }
             }
         }
+
+        val sameRepoWithoutParams = get<UserRepository>()
+
+        Log.d("hilt-demo", "user activity user same repository hash: ${sameRepoWithoutParams.hashCode()}")
     }
 }
